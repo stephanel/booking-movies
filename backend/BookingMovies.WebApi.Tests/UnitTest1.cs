9ebb2de84722cfbb8a4301f5b95c90c60773697b
Stéphane Lasmartres <slasmartres@gmail.com>
2020-12-20 00:47:11 +0100

    init .NET Core test project for the backend

---
+++ backend/BookingMovies.WebApi.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BookingMovies.WebApi.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
